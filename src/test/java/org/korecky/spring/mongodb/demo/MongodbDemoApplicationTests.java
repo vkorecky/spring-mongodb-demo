8085b986b12b1e880471bff73c196558ea005bca
Vladislav Korecky <vladislav.korecky@wandera.com>
2019-11-08 20:28:23 +0100

    Initial commit of Spring Boot project

---
+++ src/test/java/org/korecky/spring/mongodb/demo/MongodbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.korecky.spring.mongodb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
